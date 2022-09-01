8ee34f07e6f27287e0c0d9d740d3a2b6b39c3830
aossucci <antonio.ossucci@gmail.com>
2022-09-01 17:15:03 -0300

    Criado componente de ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
 import styles from './Header.module.css'
 
-import Subtitle from "../subtitle/Subtitle"
 import Title from "../title/Title"
+import Subtitle from "../subtitle/Subtitle"
+import Icon from "../icon/Icon"
 
 function Header () {
   return (
     <div className= {styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Antonio M. F. Ossucci</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/aossucci"/>
+      </div>
     </div>
   )
 }
---
+++ src/components/icon/Icon.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Icon ({iconName, size, link}) {
+  return (
+    <a href={link} target="blank">
+      <img src={`/icons/${iconName}.svg`} width={size} />
+    </a>
+  )
+}
+
+Icon.defaultProps = {
+  size: '30px'
+}
+
+export default Icon
